e5d959f46518643b74c383fac092f87fc7138afe
Mike Duin <michael.s.duin@gmail.com>
2019-01-17 00:49:26 -0800

    add youtube params

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,12 @@
+import axios from 'axios';
+
 const KEY = 'AIzaSyDheiDiIdchI4pO0CfFTgsxj7mOTUs5ico';
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    maxResults: 5,
+    key: KEY
+  }
+});
