60e64b02100313d2b6306f7d7c2c63b8ec80de1b
Mike Duin <michael.s.duin@gmail.com>
2019-01-17 00:31:31 -0800

    prevent default form behavior

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,14 +3,20 @@ import React from 'react';
 class SearchBar extends React.Component {
   state = { term: '' };
 
-  onInputChange = () => {
+  onInputChange = (e) => {
+    this.setState({ term: e.target.value });
+  };
 
+  onFormSubmit = (e) => {
+    e.preventDefault();
+
+    // TODO: make sure we call callback from parent component so that we can tell App user just submitted form
   };
 
   render () {
     return (
       <div className="search-bar ui segment">
-        <form className="ui form">
+        <form className="ui form" onSubmit={this.onFormSubmit} >
           <div className="field">
             <label> Video Search </label>
             <input type="text" value={this.state.term} onChange={this.onInputChange}/>
