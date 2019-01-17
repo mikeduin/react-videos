aca63bf68d3d8d091783d9f2c1968d251edceeec
Mike Duin <michael.s.duin@gmail.com>
2019-01-17 02:05:56 -0800

    add VideoDetail component

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,9 +1,11 @@
-// import React from 'react';
-//
-// class VideoDetail extends React.Component {
-//   render () {
-//
-//   }
-// }
-//
-// export default VideoDetail;
+import React from 'react';
+
+const VideoDetail = ({ video }) => {
+  if (!video) {
+    return <div> Loading ... </div>
+  };
+
+  return <div> {video.snippet.title} </div>
+};
+
+export default VideoDetail;
